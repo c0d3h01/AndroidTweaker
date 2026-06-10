f12e82d3683a4cc9057a45fd940c20eb621c244f
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-06-10 10:02:18 +0530

    style: formated styling - spacing

--- service.sh
+++ service.sh
@@ -1,2 +1,3 @@
 #!/system/bin/sh
+
 sleep 30 && ATweaker 2>/dev/null && exit 0
--- uninstall.sh
+++ uninstall.sh
@@ -12,6 +12,6 @@ if [ -f $INFO ]; then
         [ "$(ls -A $LINE 2>/dev/null)" ] && break 1 || rm -rf $LINE
       done
     fi
-  done < $INFO
+  done <$INFO
   rm -f $INFO
 fi
