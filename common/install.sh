cb3c7bed665e98cf4719e77b9ea6e6d45c9d50be
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-06-10 10:06:34 +0530

    refactored: install.sh - updated module. properties, update channel name.

--- common/install.sh
+++ common/install.sh
@@ -3,34 +3,35 @@ modpath="/data/adb/modules_update/AXI/"
 INJECTOR=$MODPATH/system/bin/
 
 h01print() {
-ui_print ""
-ui_print "*******************************************"
-ui_print "*          Android Tweaker                *"
-ui_print "*  Unlocking Your Device's Potential      *"
-ui_print "*******************************************"
-ui_print ""
-ui_print "[ * ] Version: 1.4.9"
-ui_print ""
-ui_print "[ * ] Supercharge Your Device's Performance"
-ui_print ""
-ui_print "[ * ] Devloped by @C0d3H01"
-ui_print ""
-ui_print "[ * ] If you found any bug give report Devloper"
-ui_print ""
-ui_print "[ * ] Channel @AndroidTweaker"
-ui_print ""
+  ui_print ""
+  ui_print "*******************************************"
+  ui_print "*          Android Tweaker                *"
+  ui_print "*  Unlocking Your Device's Potential      *"
+  ui_print "*******************************************"
+  ui_print ""
+  ui_print "[ * ] Version: 1.4.9"
+  ui_print ""
+  ui_print "[ * ] Supercharge Your Device's Performance"
+  ui_print ""
+  ui_print "[ * ] Devloped by @c0d3h01 - telegram"
+  ui_print ""
+  ui_print "[ * ] If you found any bug give report Devloper"
+  ui_print ""
+  ui_print "[ * ] Channel - @c0d3h01prjts is only official channel!"
+  ui_print ""
 }
 
 h01xz() {
-[[ "$IS64BIT" == "true" ]] && tar -xf "$MODPATH/injector2.tar.xz" -C "$MODPATH" || tar -xf "$MODPATH/injector1.tar.xz" -C "$MODPATH"
-ui_print " Injecting Components"
-mkdir -p $INJECTOR
-mv -f $MODPATH/injector/ATweaker $INJECTOR/ATweaker
- ui_print "- Injector Successfully Injected ✓"
- ui_print ""
+  [[ "$IS64BIT" == "true" ]] && tar -xf "$MODPATH/injector2.tar.xz" -C "$MODPATH" || tar -xf "$MODPATH/injector1.tar.xz" -C "$MODPATH"
+  ui_print " Injecting Components"
+  mkdir -p $INJECTOR
+  mv -f $MODPATH/injector/ATweaker $INJECTOR/ATweaker
+  ui_print "- Injector Successfully Injected ✓"
+  ui_print ""
 }
 h01xz
 h01print
 note="Boost your device's performance and enjoy a smoother experience with our optimizer, designed specifically for $(getprop ro.product.device)!"
 sed -i "/description=/c description=$note" "${modpath}module.prop"
-nohup am start -a android.intent.action.VIEW -d https://t.me/Androidtweaker >/dev/null 2>&1 & >/dev/null 2>&1 &
+nohup am start -a android.intent.action.VIEW -d https://t.me/c0d3h01prjts >/dev/null 2>&1 &
+>/dev/null 2>&1 &
